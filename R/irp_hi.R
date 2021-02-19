429648f2058c2e97b16a28d40c6a8e24fcb91d52
henningte <34153309+henningte@users.noreply.github.com>
2021-02-19 08:15:33 +0100

    Added the possibility to compute custom humification indices.

--- R/irp_hi.R
+++ R/irp_hi.R
@@ -1,40 +1,102 @@
 #' Humification indices from mid infrared spectra.
 #'
-#' \code{irp_hi} computes various humification indices as reported by
-#' \insertCite{Broder.2012;textual}{irpeat} for mid infrared
-#' spectra with the x values representing wavenumber values (no checks are performed):
-#' The following humification indices are computed (values
-#' represent wavenumbers [cm\eqn{^{-1}}]) \insertCite{Broder.2012}{irpeat}:
+#' \code{irp_hi} computes either custom humification indices or a predefined set of
+#' humification indices as reported by \insertCite{Broder.2012;textual}{irpeat}
+#' for mid infrared spectra with the x values representing wavenumber values
+#' (no checks are performed) (see the details section for the humification indices
+#' computed by default). A humification index is the ratio of the intensity
+#' values at two different x axis values (e.g. wavenumbers) and defined as:
+#' \deqn{
+#'   HI = \frac{y_x1}{y_x2}
+#' },
+#' where \eqn{y_x1} is the intensity of the spectrum at x axis value \eqn{x1} and
+#' \eqn{y_x2} is the intensity of the spectrum at x axis value \eqn{x2}.
+#'
+#' @details
+#' The following humification indices are computed by
+#' default (if \code{x1 = NULL} and \code{x2 = NULL}) (values
+#' represent wavenumbers [cm\eqn{^{-1}}]) \insertCite{Broder.2012}{irpeat} :
 #' \describe{
-#'   \item{1420/1090}{OH and CO of phenols or CH of CH{_1} and CH{_3}
+#'   \item{hi1: 1420/1090}{OH and CO of phenols or CH of CH{_1} and CH{_3}
 #'   groups (phenolic and aliphatic structures)/polysaccharides}
-#'   \item{1510/1090}{Aromatic C=C or C=O of amides/polysaccharides}
-#'   \item{1630/1090}{Aromatic C=C and COO\eqn{^{-1}} (aromatics and aromatic
+#'   \item{hi2: 1510/1090}{Aromatic C=C or C=O of amides/polysaccharides}
+#'   \item{hi3: 1630/1090}{Aromatic C=C and COO\eqn{^{-1}} (aromatics and aromatic
 #'   or aliphatic carboxylates)/polysaccharides}
-#'   \item{1720/1090}{carbonylic and carboxylic C=O (carboxylic acids and
+#'   \item{hi4: 1720/1090}{carbonylic and carboxylic C=O (carboxylic acids and
 #'   aromatic esters)/polysaccharides}
 #' }
 #'
 #' @param x An object of class \code{\link[ir:ir_new_ir]{ir}}.
-#' @return An object of class \code{\link[ir:ir_new_ir]{ir}} with three new
-#' columns (hi1, hi2, hi3 and hi4) that correspond to the humification indices
-#' defined in the description.
+#' @param x1 A numeric vector with values representing x axis values in the spectra
+#' of \code{x}. This is \eqn{x1} in the equation displayed above. If
+#' multiple humification indices for the same \code{x2} should be computed, \code{x1}
+#' can contain multiple values. If \code{x1 = NULL},
+#' the default humification indices will be computed (see details section).
+#' @param x2 A numeric value representing an x axis value in the spectra
+#' of \code{x}. This is \eqn{x2} in the equation displayed above. If \code{x2 = NULL},
+#' the default humification indices will be computed (see details section).
+#' @return An object of class \code{\link[ir:ir_new_ir]{ir}} with additional
+#' columns for additional humification indices. If \code{x1 = NULL} and \code{x2 = NULL},
+#' these are four new columns (hi1, hi2, hi3 and hi4) that correspond to the humification
+#' indices defined in the details section. If \code{x1} and \code{x2} are not \code{NULL},
+#' the columns have names \code{"hi_x1_x2"} where \code{x1} and \code{x2} are replaced by the
+#' respective values.
+#' @examples
+#' # get sample data
+#' library(ir)
+#'
+#' # compute default humification indices
+#' d <- ir_sample_data
+#' d <- irp_hi(d)
+#'
+#' # compute custom humification index
+#' d <- ir_sample_data
+#' d <- irp_hi(d, x1 = 2900, x2 = 1090)
+#'
+#' # compute custom humification indices
+#' d <- ir_sample_data
+#' d <- irp_hi(d, x1 = c(2900, 1630), x2 = 1090)
+#'
 #' @references
 #'   \insertAllCited{}
 #' @export
-irp_hi <- function(x) {
+irp_hi <- function(x, x1 = NULL, x2 = NULL) {
 
+  default <- TRUE
   ir::ir_check_ir(x)
+  if((is.null(x1) && !is.null(x2)) || (!is.null(x1) && is.null(x2))) {
+    rlang::abort("Only one of `x1` and `x2` is `NULL`. Both of `x1` and `x2` must be `NULL` if you want to compute the default humification indices. If you want to compute custom humification indices, both `x1` and `x2` must be numeric vectors.")
+  }
+  if(!is.null(x1) && !is.null(x2)) {
+    if(!is.numeric(x1)) {
+      rlang::abort("`x1` must be numeric or `NULL`.")
+    }
+    if(!is.numeric(x2)) {
+      rlang::abort("`x2` must be numeric or `NULL`.")
+    }
+    cond <- length(x2)
+    if(cond != 1) {
+      rlang::abort(paste0("x2 must be a numeric value, but is of length ", cond, "."))
+    }
+    default <- FALSE
+  } else {
+   x1 <- c(1420, 1510, 1630, 1720)
+   x2 <- 1090
+  }
+
+  hi_wn <- data.frame(numerator = x1,
+                      denominator = rep(x2, length(x1)),
+                      stringsAsFactors = FALSE)
   x_flat <- ir::ir_flatten(x)
-  hi_wn <- data.frame(numerator = c(1420, 1510, 1630, 1720),
-                   denominator = c(1090, 1090, 1090, 1090),
-                   stringsAsFactors = FALSE)
   hi_index <- data.frame(
     numerator = ir::ir_get_wavenumberindex(x_flat, wavenumber = hi_wn$numerator, warn = TRUE),
     denominator = ir::ir_get_wavenumberindex(x_flat, wavenumber = hi_wn$denominator, warn = TRUE),
     stringsAsFactors = FALSE)
   hi <- as.data.frame(t(x_flat[hi_index$numerator, -1]/x_flat[hi_index$denominator, -1]))
-  colnames(hi) <- paste0("hi", seq_len(ncol(hi)))
+  colnames(hi) <- paste0("hi_", x1, "_", x2)
+  if(default) {
+    colnames(hi) <- paste0("hi", seq_len(ncol(hi)))
+  }
   dplyr::bind_cols(x, hi)
 
 }
