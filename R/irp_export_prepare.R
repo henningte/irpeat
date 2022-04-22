a15d92077e81c33cbaa4193776502356ca8a6ba9
henningte <34153309+henningte@users.noreply.github.com>
2022-04-22 12:31:08 +0200

    Update `irp_export_prepare()`: Deprecate.

--- R/irp_export_prepare.R
+++ R/irp_export_prepare.R
@@ -1,23 +1,34 @@
 #' Stores errors and units of error or units columns of objects of class `ir` in separate columns
 #'
-#' `irp_export_prepare` stores errors ([errors::errors]) and
-#' units ([`units`][units::set_units]) of error or
-#' units columns in an object of class [`ir`][ir::ir_new_ir] in separate columns.
+#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
+#' `irp_export_prepare` stores errors ([errors::errors]) and units
+#' ([`units`][units::set_units]) of error or units columns in an object of
+#' class `ir` in separate columns.
 #'
 #' @param x An object of class [`ir`][ir::ir_new_ir].
 #'
-#' @return An object of class [`ir`][ir::ir_new_ir] with a new column for
-#' each column that is of class `"units"` and each column that is of class
-#' `"errors"`. New columns are named old_column_name_errors and old_column_name_units,
-#' respectively.
+#' @return An object of class `ir` with a new column for each column that is of class
+#' `units` and each column that is of class `errors`. New columns are named
+#' `old_column_name_errors` and `old_column_name_units`, respectively.
+#'
+#' @keywords internal
 #'
 #' @examples
 #' library(ir)
 #'
-#' irp_export_prepare(ir::ir_sample_data[1:5, ])
+#' irp_export_prepare(ir::ir_sample_data[1:5, ]) # now deprecated
+#'
+#' # replacement for deprecated function:
+#' \dontrun{
+#' ir::ir_export_prepare(ir::ir_sample_data[1:5, ], what = "metadata")
+#' }
 #'
 #' @export
 irp_export_prepare <- function(x) {
+
+  lifecycle::deprecate_warn("0.1.0", "irp_export_prepare()", "ir::ir_export_prepare()")
 
   stopifnot(inherits(x, "ir"))
 
@@ -26,32 +37,44 @@ irp_export_prepare <- function(x) {
                   quantities = purrr::map_lgl(x, inherits, "quantities"),
                   stringsAsFactors = FALSE)
 
-  d_errors <- purrr::map2(d$errors, seq_along(d$errors), function(y, z) {
-    if(y) {
-      d <- data.frame(x = quantities::quantities(x[, z, drop = TRUE])$errors,
-                 stringsAsFactors = FALSE)
-      colnames(d) <- paste0(colnames(x)[[z]], "_errors")
-      d
-    }
-  })
+  if(!any(d$errors)) {
+    d_errors <- NULL
+  } else {
+    d_errors <- purrr::map2_dfc(d$errors, seq_along(d$errors), function(.y, .z) {
+      if(.y) {
+        d <- data.frame(x = quantities::quantities(x[, .z, drop = TRUE])$errors,
+                        stringsAsFactors = FALSE)
+        colnames(d) <- paste0(colnames(x)[[.z]], "_errors")
+        d
+      }
+    })
+  }
 
-  d_units_units <- purrr::map2(d$units  & !d$quantities, seq_along(d$quantities), function(y, z) {
-    if(y) {
-      d <- data.frame(x = rep(as.character(attr(x[, z, drop = TRUE], "units")), nrow(x)),
-                      stringsAsFactors = FALSE)
-      colnames(d) <- paste0(colnames(x)[[z]], "_units")
-      d
-    }
-  })
+  if(!any(d$units  & !d$quantities)) {
+    d_units_units <- NULL
+  } else {
+    d_units_units <- purrr::map2_dfc(d$units  & !d$quantities, seq_along(d$quantities), function(.y, .z) {
+      if(.y) {
+        d <- data.frame(x = rep(as.character(attr(x[, .z, drop = TRUE], "units")), nrow(x)),
+                        stringsAsFactors = FALSE)
+        colnames(d) <- paste0(colnames(x)[[.z]], "_units")
+        d
+      }
+    })
+  }
 
-  d_quantities_units <- purrr::map2(d$quantities, seq_along(d$quantities), function(y, z) {
-    if(y) {
-      d <- data.frame(x = rep(as.character(quantities::quantities(x[, z, drop = TRUE])$units), nrow(x)),
-                      stringsAsFactors = FALSE)
-      colnames(d) <- paste0(colnames(x)[[z]], "_units")
-      d
-    }
-  })
+  if(!any(d$quantities)) {
+    d_quantities_units <- NULL
+  } else {
+    d_quantities_units <- purrr::map2_dfc(d$quantities, seq_along(d$quantities), function(.y, .z) {
+      if(.y) {
+        d <- data.frame(x = rep(as.character(quantities::quantities(x[, .z, drop = TRUE])$units), nrow(x)),
+                        stringsAsFactors = FALSE)
+        colnames(d) <- paste0(colnames(x)[[.z]], "_units")
+        d
+      }
+    })
+  }
 
   dplyr::bind_cols(x, d_errors, d_units_units, d_quantities_units)
 
