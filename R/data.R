bf54e013d82adc7241fd6820c5939be163ccff3e
henningte <34153309+henningte@users.noreply.github.com>
2025-07-16 14:54:15 +0200

    Fix links in documentation.

--- R/data.R
+++ R/data.R
@@ -3,6 +3,6 @@
 #' Sample transmission mid infrared spectra of peat samples from the 'redoxpeat'
 #' package.
 #'
-#' @format An [ir](ir::ir_new_ir) object with 59 samples. See
+#' @format An [`ir`][`ir::ir_new_ir()`] object with 59 samples. See
 #' `?redoxpeat::d_mir` for additional information.
 "irpeat_sample_data"
